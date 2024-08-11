02ee66d5b4436ad0fa8afa58a517cf7895b4d193
mohamed_amine <med.amine.jamal3@gmail.com>
2024-08-11 19:50:45 +0200

    finishing the main project

--- file.js
+++ file.js
@@ -1,17 +1,54 @@
 
 const container = document.querySelector('#container');
-for(let i = 0; i < 16; i++){
-    const subcontainer = document.createElement('div');
-    subcontainer.className = "subcontainer";
-    for(let j=0;j < 16;j++){
-        const gridItem = document.createElement('div');
-        gridItem.className = "gridItem"
-        subcontainer.appendChild(gridItem);
+const createGrid = document.getElementById("createGrid");
+
+function createNewGrid(size){
+for(let i = 0; i < size*size; i++){
+    const gridItem = document.createElement('div');
+    gridItem.className = "gridItem";
+    gridItem.style.width = `${100 / size}%`;
+    gridItem.style.height = `${100 / size}%`;
+    container.appendChild(gridItem);
+}}
+function removeCurrentGrid(item){
+    item.forEach(div =>{
+        div.remove();
+    })
+
+}
+    createNewGrid(16);
+    function newSetup(){
+        let items = document.querySelectorAll(".gridItem");
+        let s = prompt("enter number of squares per side");
+        while(s > 100){
+            s =prompt("number invalid , the number should be less than 100");
+        }
+        removeCurrentGrid(items);
+        createNewGrid(s);
+        items = document.querySelectorAll(".gridItem");
+        items.forEach(div =>{
+            div.addEventListener('mouseover',function(){
+                div.style.backgroundColor= "black";
+            })
+            })
+        
+
     }
-    container.appendChild(subcontainer);
-}
-const items = document.querySelector(".gridItem");
-function hover(){
-    items.style.color ="black";
-}
-items.addEventListener("mouseover",()=>hover());
+    createGrid.addEventListener("click",()=>newSetup());
+    const items = document.querySelectorAll(".gridItem");
+    items.forEach(div =>{
+    div.addEventListener('mouseover',function(){
+        div.style.backgroundColor= "black";
+    })
+    })
+    function clear(){
+        const items = document.querySelectorAll(".gridItem");
+        items.forEach(div =>{
+            div.style.backgroundColor="white";
+            div.addEventListener('mouseover',function(){
+                div.style.backgroundColor="black";
+            })
+        })
+    }
+    const clearButton = document.getElementById("clear");
+    clearButton.addEventListener("click",()=>clear());
